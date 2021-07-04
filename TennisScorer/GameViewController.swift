110344ed1d64fc346b109361747e8398536e4486
Sophie <sophienanterre@gmail.com>
2021-07-04 18:38:49 +0200

    WIP mise en place de la logique pour déterminer qui a gagné

--- TennisScorer/GameViewController.swift
+++ TennisScorer/GameViewController.swift
@@ -39,16 +39,13 @@ class GameViewController: UIViewController {
     @IBOutlet weak var scoreSet5P1: UILabel!
     
     @IBOutlet weak var scoreSet5P2: UILabel!
-    
-    @IBOutlet weak var scoreSet6P1: UILabel!
-    
-    @IBOutlet weak var scoreSet6P2: UILabel!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-    
+   
     //MARK: - PROPERTIES
     
     var CounterGameP1: Int = 0
@@ -68,10 +65,136 @@ class GameViewController: UIViewController {
     var wonSetP1: Int = 0
     var wonSetP2: Int = 0
     
+    
+    
     //MARK: - HELPERS
     
- 
+    // TODO : gérer best of 3 et best of 5
     
+    // Pour gagner, un joueur doit avoir gagné 2 sets
+//    func whoWonBestOf3() {
+//        if counterSet1P1 == 6 {
+//            wonSetP1 += 1
+//        }
+//
+//        if counterSet2P1 == 6 {
+//            wonSetP1 += 1
+//        }
+//
+//        if counterSet3P1 == 6 {
+//            wonSetP1 += 1
+//        }
+//
+//        if counterSet1P2 == 6 {
+//            wonSetP2 += 1
+//        }
+//
+//        if counterSet2P2 == 6 {
+//            wonSetP2 += 1
+//        }
+//
+//        if counterSet3P2 == 6 {
+//            wonSetP2 += 1
+//        }
+//
+//        if wonSetP1 == 2 {
+//            // alert
+//            let alert = UIAlertController(title: "Match terminé",
+//                                          message: "Le vainqueur est le joueur 1",
+//                                          preferredStyle: .alert)
+//            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+//            self.present(alert, animated: true)
+//        }
+//
+//        if wonSetP2 == 2 {
+//            // alert
+//            let alert = UIAlertController(title: "Match terminé",
+//                                          message: "Le vainqueur est le joueur 2",
+//                                          preferredStyle: .alert)
+//            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+//            self.present(alert, animated: true)
+//
+//            btnPlayer1.isHidden = true
+//            btnPlayer2.isHidden = true
+//
+//        }
+//    }
+    
+    // Pour gagner un joueur doit avoir gagné 3 sets
+    func whoWonBestOf5() {
+        var player1ScoresArray: [String?] = [scoreSet1P1.text, scoreSet2P1.text, scoreSet3P1.text, scoreSet4P1.text, scoreSet5P1.text]
+        
+        for i 
+
+        if scoreSet1P1.text == "6" {
+            wonSetP1 += 1
+        }
+        
+        if scoreSet2P1.text == "6" {
+            wonSetP1 += 1
+        }
+        
+        if scoreSet3P1.text == "6" {
+            wonSetP1 += 1
+        }
+        
+        if scoreSet4P1.text == "6" {
+            wonSetP1 += 1
+        }
+        
+        if scoreSet5P1.text == "6" {
+            wonSetP1 += 1
+        }
+      
+        
+        if scoreSet1P1.text == "6" {
+            wonSetP2 += 1
+        }
+        
+        if scoreSet2P1.text == "6" {
+            wonSetP2 += 1
+        }
+        
+        if scoreSet3P1.text == "6" {
+            wonSetP2 += 1
+        }
+        
+        if scoreSet4P1.text == "6" {
+            wonSetP2 += 1
+        }
+        
+        if scoreSet5P1.text == "6" {
+            wonSetP2 += 1
+        }
+        
+
+        if wonSetP1 == 3 {
+            // alert
+            let alert = UIAlertController(title: "Match terminé",
+                                          message: "Le vainqueur est le joueur 1",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
+        
+        if wonSetP2 == 3 {
+            // alert
+            let alert = UIAlertController(title: "Match terminé",
+                                          message: "Le vainqueur est le joueur 2",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            
+            btnPlayer1.isHidden = true
+            btnPlayer2.isHidden = true
+        }
+
+        print("number of set won by 1st player : \(wonSetP1)")
+        print("number of set won by 2nd player: \(wonSetP2)")
+  
+    }
+    
+    //MARK: - IBACTIONS
     
     @IBAction func player1ScoreButtonPressed(_ sender: UIButton) {
         CounterGameP1 += 1
@@ -96,35 +219,30 @@ class GameViewController: UIViewController {
             
             if counterSet1P1 != 6 && counterSet1P2 != 6 {
                 counterSet1P1 += 1
-                print("P1 \(counterSet1P1)")
                 scoreSet1P1.text = String(counterSet1P1)
             }
             
             if (counterSet1P1 == 6 || counterSet1P2 == 6) && (counterSet2P1 != 6 && counterSet2P2 != 6) {
                 counterSet2P1 += 1
-                print("P1 \(counterSet2P1)")
                 scoreSet2P1.text = String(counterSet2P1)
             }
             
             if (counterSet2P1 == 6 || counterSet2P2 == 6) && (counterSet3P1 != 6 && counterSet3P2 != 6) {
                 counterSet3P1 += 1
-                print("P1 \(counterSet3P1)")
                 scoreSet3P1.text = String(counterSet3P1)
             }
             
             if (counterSet3P1 == 6 || counterSet3P2 == 6) && (counterSet4P1 != 6 && counterSet4P2 != 6) {
                 counterSet4P1 += 1
-                print("P1 \(counterSet4P1)")
                 scoreSet4P1.text = String(counterSet4P1)
             }
             
             if (counterSet4P1 == 6 || counterSet4P2 == 6) && (counterSet5P1 != 6 && counterSet5P2 != 6) {
                 counterSet5P1 += 1
-                print("P1 \(counterSet5P1)")
                 scoreSet5P1.text = String(counterSet5P1)
             }
-            
-            
+
+            whoWonBestOf5()
         }
     }
     
@@ -151,40 +269,32 @@ class GameViewController: UIViewController {
             
             if counterSet1P1 != 6 && counterSet1P2 != 6 {
                 counterSet1P2 += 1
-                print("P2 \(counterSet1P2)")
                 scoreSet1P2.text = String(counterSet1P2)
             }
             
             if (counterSet1P1 == 6 || counterSet1P2 == 6) && (counterSet2P1 != 6 && counterSet2P2 != 6) {
                 counterSet2P2 += 1
-                print("P2 \(counterSet2P2)")
                 scoreSet2P2.text = String(counterSet2P2)
             }
             
             if (counterSet2P1 == 6 || counterSet2P2 == 6) && (counterSet3P1 != 6 && counterSet3P2 != 6) {
                 counterSet3P2 += 1
-                print("P2 \(counterSet2P2)")
                 scoreSet3P2.text = String(counterSet3P2)
             }
             
             if (counterSet3P1 == 6 || counterSet3P2 == 6) && (counterSet4P1 != 6 && counterSet4P2 != 6) {
                 counterSet4P2 += 1
-                print("P2 \(counterSet2P2)")
                 scoreSet4P2.text = String(counterSet4P2)
             }
             
             if (counterSet4P1 == 6 || counterSet4P2 == 6) && (counterSet5P1 != 6 && counterSet5P2 != 6) {
                 counterSet5P2 += 1
-                print("P2 \(counterSet5P2)")
                 scoreSet5P2.text = String(counterSet5P2)
             }
-        
-            
-            
-               
-        }
-        
 
+            
+            whoWonBestOf5()
+            
+        }
     }
-    
 }
