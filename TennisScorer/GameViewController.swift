83991f42dd8f309e5098a67529787af5dd30a0c7
Sophie <sophienanterre@gmail.com>
2021-07-11 19:33:10 +0200

    refactoring

--- TennisScorer/GameViewController.swift
+++ TennisScorer/GameViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
     
-//    MARK: - IBOUTLETS
-
+    
+    //    MARK: - IBOUTLETS
+    
     @IBOutlet weak var btnPlayer1: UIButton!
     
     @IBOutlet weak var btnPlayer2: UIButton!
@@ -27,25 +27,25 @@ class GameViewController: UIViewController {
     @IBOutlet weak var scoreSet2P1: UILabel!
     
     @IBOutlet weak var scoreSet2P2: UILabel!
-
+    
     @IBOutlet weak var scoreSet3P1: UILabel!
-
+    
     @IBOutlet weak var scoreSet3P2: UILabel!
-
+    
     @IBOutlet weak var scoreSet4P1: UILabel!
-
+    
     @IBOutlet weak var scoreSet4P2: UILabel!
     
     @IBOutlet weak var scoreSet5P1: UILabel!
     
     @IBOutlet weak var scoreSet5P2: UILabel!
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-   
+    
     //MARK: - PROPERTIES
     
     var CounterGameP1: Int = 0
@@ -65,14 +65,14 @@ class GameViewController: UIViewController {
     
     var tennisScorer: TennisScorer?
     
-
+    
     
     //MARK: - HELPERS
     
     var currentSet: Int = 0
     
     func updateLabels() {
-                
+        
         scoreSet1P1.text = String(sharedTennisScorer!.score.sets[0].0)
         
         scoreSet2P1.text = String(sharedTennisScorer!.score.sets[1].0)
@@ -99,17 +99,50 @@ class GameViewController: UIViewController {
         var player2ScoresArray = [scoreSet1P2.text!, scoreSet2P2.text!, scoreSet3P2.text!, scoreSet4P2.text!, scoreSet5P2.text!]
         
         
-       
+        if sharedTennisScorer!.isOver == false {
+            do {
+                let result = try sharedTennisScorer!.winner()
+                
+                if result == 1 {
+                    sharedTennisScorer!.isOver = true
+                    
+                    let alert = UIAlertController(title: "Match terminé",
+                                                  message: "Le vainqueur est le joueur 1",
+                                                  preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                    self.present(alert, animated: true)
+                    
+                    btnPlayer1.isHidden = true
+                    btnPlayer2.isHidden = true
+                }
+                
+                if result == 2 {
+                    sharedTennisScorer!.isOver = true
+                    
+                    let alert = UIAlertController(title: "Match terminé",
+                                                  message: "Le vainqueur est le joueur 2",
+                                                  preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                    self.present(alert, animated: true)
+                    
+                    btnPlayer1.isHidden = true
+                    btnPlayer2.isHidden = true
+                }
+            } catch  {
+                
+            }
+            
+        }
         
     }
-
-   //MARK: - VALUES LABELS
-//    var player1ScoresArray = [scoreSet1P1.text, scoreSet2P1.text, scoreSet3P1.text, scoreSet4P1.text, scoreSet5P1.text]
-//
-//    var player2ScoresArray = [scoreSet1P2.text, scoreSet2P2.text, scoreSet3P2.text, scoreSet4P2.text, scoreSet5P2.text]
+    
+    //MARK: - VALUES LABELS
+    //    var player1ScoresArray = [scoreSet1P1.text, scoreSet2P1.text, scoreSet3P1.text, scoreSet4P1.text, scoreSet5P1.text]
+    //
+    //    var player2ScoresArray = [scoreSet1P2.text, scoreSet2P2.text, scoreSet3P2.text, scoreSet4P2.text, scoreSet5P2.text]
     
     //MARK: - IBACTIONS
-
+    
     
     @IBAction func player1ScoreButtonPressed(_ sender: UIButton) {
         CounterGameP1 += 1
@@ -126,24 +159,52 @@ class GameViewController: UIViewController {
         if (CounterGameP1 == 3) {
             currentScoreP1.text = GameScore.forty.rawValue
         }
-
+        
         if (CounterGameP1 == 4) {
+            if CounterGameP2 == 4 {
+                CounterGameP1 = 3
+                CounterGameP2 = 3
+                currentScoreP1.text = GameScore.forty.rawValue
+                currentScoreP2.text = GameScore.forty.rawValue
+            }
+            else {
+                if CounterGameP2 == 3 {
+                    currentScoreP1.text = GameScore.advantage.rawValue
+                }
+                else {
+                    CounterGameP1 = 0
+                    CounterGameP2 = 0
+                    currentScoreP1.text = GameScore.love.rawValue
+                    currentScoreP2.text = GameScore.love.rawValue
+                    
+                    do {
+                        try  sharedTennisScorer!.updateWithPointsByPlayer(1)
+                        if sharedTennisScorer!.updateLabel == true {
+                            updateLabels()
+                        }
+                    } catch  {
+                        //
+                    }
+                }
+            }
+        }
+        if (CounterGameP1 == 5) {
             CounterGameP1 = 0
+            CounterGameP2 = 0
             currentScoreP1.text = GameScore.love.rawValue
             currentScoreP2.text = GameScore.love.rawValue
-
+            
             do {
                 try  sharedTennisScorer!.updateWithPointsByPlayer(1)
                 if sharedTennisScorer!.updateLabel == true {
                     updateLabels()
                 }
             } catch  {
-                
+                //
             }
-
         }
     }
-
+    
     
     @IBAction func player2BtnPressed(_ sender: Any) {
         CounterGameP2 += 1
@@ -162,21 +223,48 @@ class GameViewController: UIViewController {
         }
         
         if (CounterGameP2 == 4) {
+            if CounterGameP1 == 4 {
+                CounterGameP1 = 3
+                CounterGameP2 = 3
+                currentScoreP1.text = GameScore.forty.rawValue
+                currentScoreP2.text = GameScore.forty.rawValue
+            }
+            else {
+                if CounterGameP1 == 3 {
+                    currentScoreP2.text = GameScore.advantage.rawValue
+                }
+                else {
+                    CounterGameP2 = 0
+                    CounterGameP1 = 0
+                    currentScoreP2.text = GameScore.love.rawValue
+                    currentScoreP1.text = GameScore.love.rawValue
+                    
+                    do {
+                        try  sharedTennisScorer!.updateWithPointsByPlayer(2)
+                        if sharedTennisScorer!.updateLabel == true {
+                            updateLabels()
+                        }
+                    } catch  {
+                        //
+                    }
+                }
+            }
+        }
+        if (CounterGameP2 == 5) {
             CounterGameP2 = 0
-            currentScoreP1.text = GameScore.love.rawValue
+            CounterGameP1 = 0
             currentScoreP2.text = GameScore.love.rawValue
-
+            currentScoreP1.text = GameScore.love.rawValue
+            
             do {
                 try  sharedTennisScorer!.updateWithPointsByPlayer(2)
                 if sharedTennisScorer!.updateLabel == true {
                     updateLabels()
                 }
             } catch  {
-                
+                //
             }
-
-                
         }
     }
+    
 }
-
